315571249dca66d5bca39b3fcb76f85966ae0feb
ivan <ivankohquanwei@gmail.com>
2022-06-30 00:45:49 +0800

    ssl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,8 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
+
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
@@ -11,8 +13,11 @@ module.exports = ({ env }) => ({
       user: config.user,
       password: config.password,
       ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
+    },
+      options: {
+      ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
   },
